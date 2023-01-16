cc3efe7f4eab306c9f49695b81c769ab0b656888
Elmir <duck-3@mail.ru>
2023-01-16 22:58:01 +0300

    Initial Commit

---
+++ Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by Эльмир Юсупов on 16.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
